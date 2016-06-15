90e114e057a2c3cc8c7aeef2bb8dfa48e3329ce2
Christian Sakai <christianmsakai@gmail.com>
2016-06-15 18:56:47 -0400

    update

--- phoenix_commerce/test/acceptance/products_test.exs
+++ phoenix_commerce/test/acceptance/products_test.exs
@@ -7,20 +7,25 @@ defmodule PhoenixCommerce.Acceptance.ProductsTest do
 
   hound_session
 
+  setup do
+    Repo.delete_all(Product)
+
+    {:ok, product} = %Product{
+        name: "Some product",
+        description: "Some product description",
+        price: Decimal.new("25.20")
+      } |> Repo.insert
+
+    {:ok, product: product}
+  end
+
   test "/products has a list of products" do
     navigate_to "/products"
 
     assert find_element(:css, "ul.products")
   end
 
-  test "/products - productd details include name, description, and price" do
-    {:ok, product} =
-      %Product{
-        name: "Some product",
-        description: "Some product description",
-        price: Decimal.new("25.20")
-      } |> Repo.insert
-
+  test "/products - productd details include name, description, and price", %{product: product} do
     navigate_to "/products"
 
     product_li = find_element(:css, "ul.products li")
@@ -31,5 +36,16 @@ defmodule PhoenixCommerce.Acceptance.ProductsTest do
     assert visible_text(name) == product.name
     assert visible_text(price) == "$ #{product.price}"
     assert visible_text(description) == product.description
+  end
+
+  test "/products - click a product to view its details", %{product: product} do
+    navigate_to "/products"
+
+    product_li = find_element(:css, "ul.products li")
+    link = find_within_element(product_li, :css, "a")
+
+    click(link)
+
+    assert "/products/#{product.id}" == current_path 
   end
 end
